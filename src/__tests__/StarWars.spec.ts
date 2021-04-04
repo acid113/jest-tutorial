c84df20ee76eea1f63745dd8ace0a01b6e110291
Marvin Estacio <marvin.estacio@merchantspring.com.au>
2021-04-04 21:09:29 +0800

    mock API calls

--- src/__tests__/StarWars.spec.ts
+++ src/__tests__/StarWars.spec.ts
@@ -1,38 +1,50 @@
 import { fetchPeople, fetchPerson } from "../api/StarWars";
+const axios = require("axios"); // * 'import axios' didn't work
 
-// const person1 = {
-// 	name: "Luke Skywalker",
-// 	gender: "male",
-// };
+const person1 = {
+	name: "Luke Skywalker Mock",
+	gender: "male",
+};
 
-// const person2 = {
-// 	name: "R2-D2",
-// 	gender: "n/a",
-// };
+const person2 = {
+	name: "R2-D2 Mock",
+	gender: "n/a",
+};
 
-// const people = [
-// 	{
-// 		...person1,
-// 		...person2,
-// 	},
-// ];
+const people = [person1, person2];
 
-// const fetchPerson = jest.fn().mockReturnValue(person1);
-// const fetchPeople = jest.fn().mockReturnValue(people);
-
-// jest.mock("../api/StarWars", () => ({
-// 	fetchPerson,
-// 	fetchPeople,
-// }));
+// * mocking modules
+jest.mock("axios");
 
 describe("Star Wars API", () => {
-	test("should get people", async () => {
+	test("should get a person", async () => {
+		// ? doesn't work with 'import'
+		axios.request.mockImplementationOnce(() =>
+			Promise.resolve({
+				data: person1,
+			})
+		);
+
 		const person = await fetchPerson("1");
 
 		expect(person).not.toBeNull();
-		expect(person.name).toEqual("Luke Skywalker");
-		expect(person.gender).toEqual("male");
+		expect(person.name).toEqual(person1.name);
+		expect(person.gender).toEqual(person1.gender);
 	});
 
-	// test("should get a person by ID", () => {});
+	test("should get people", async () => {
+		axios.request.mockImplementationOnce(() =>
+			Promise.resolve({
+				data: {
+					results: people,
+				},
+			})
+		);
+
+		const personList = await fetchPeople();
+
+		expect(personList).not.toBeNull();
+		expect(personList[1].name).toEqual(person2.name);
+		expect(personList[1].gender).toEqual(person2.gender);
+	});
 });
