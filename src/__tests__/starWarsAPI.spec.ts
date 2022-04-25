c3339ec766340a220a39ef4326778c5aa8e87b0d
Marvin Estacio <maverick_mle@yahoo.com>
2022-04-25 18:46:05 +0800

    added using spyOn

--- src/__tests__/starWarsAPI.spec.ts
+++ src/__tests__/starWarsAPI.spec.ts
@@ -19,26 +19,29 @@ const mockedAxios = axios as jest.Mocked<typeof axios>
 
 describe("Star Wars API", () => {
 	test("should get a person", async () => {
+		// * Option 1: using jest.Mocked
 		mockedAxios.request.mockResolvedValueOnce({
 			data : person1
 		});
 
 		const person = await fetchPerson("1");
-
+		
 		expect(person).not.toBeNull();
 		expect(person.name).toEqual(person1.name);
 		expect(person.gender).toEqual(person1.gender);
 	});
 
 	test("should get people", async () => {
-		mockedAxios.request.mockResolvedValueOnce({
-			data : {
+		// * Option 2: using jest.spyOn
+		const spyRequest = jest.spyOn(axios, 'request').mockResolvedValue({
+			data: {
 				results: people
 			}
 		});
 
 		const personList = await fetchPeople();
 
+		expect(spyRequest).toHaveBeenCalled();
 		expect(personList).not.toBeNull();
 		expect(personList[1].name).toEqual(person2.name);
 		expect(personList[1].gender).toEqual(person2.gender);
