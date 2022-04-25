ac10af0e81c407691ad64cf902a6c0217a080b8c
Marvin Estacio <maverick_mle@yahoo.com>
2022-04-25 18:45:49 +0800

    updated config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+const config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-};
+  clearMocks: true,
+  testMatch: [
+    '<rootDir>/src/__tests__/**/*.ts'
+  ]
+}
+module.exports = config;
